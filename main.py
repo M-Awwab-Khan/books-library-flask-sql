047e56c46a812fb9786ce02f166fd3dc0165d729
Awwab <mawwabkhank2006@gmail.com>
2024-02-15 18:24:31 +0500

    separated database logic

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float
+from book_model import db
 
 app = Flask(__name__)
 
-class Base(DeclarativeBase):
-    pass
-
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
-db = SQLAlchemy(model_class=Base)
+
 db.init_app(app)
 
-class Book(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
-    author: Mapped[str] = mapped_column(String(250), nullable=False)
-    rating: Mapped[float] = mapped_column(Float, nullable=False)
- 
 with app.app_context():
     db.create_all()
-
 
 all_books = []
 @app.route('/')
