41f1c98d9d5f949f1338631dc2eeda52504249b4
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-15 13:45:55 +0500

    create db with sqlite

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+db = sqlite3.connect("books-collection.db")
+cursor = db.cursor()
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# db.commit()
 
 app = Flask(__name__)
 
